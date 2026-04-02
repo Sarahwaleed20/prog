0cd4efbbfd470526cafc194cc7b374e0842de7fb
Sarahwaleed20 <sh2400281@tkh.edu.eg>
2026-04-02 14:51:56 +0200

    milestone 2 last edit

--- client.c
+++ client.c
@@ -12,86 +12,77 @@
 #define BUFFER_SIZE 1024
 #define PSK "SuperSecretKey123SuperSecretKey12"
 
-int encrypt(unsigned char *plaintext, int plaintext_len,
-            unsigned char *key, unsigned char *iv,
-            unsigned char *ciphertext) {
+void hash_password(const char *password, char *output_buffer) {
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
+    EVP_MD_CTX *context = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(context, EVP_sha256(), NULL);
+    EVP_DigestUpdate(context, password, strlen(password));
+    EVP_DigestFinal_ex(context, hash, &hash_len);
+    EVP_MD_CTX_free(context);
+    for(int i = 0; i < hash_len; i++) sprintf(output_buffer + (i * 2), "%02x", hash[i]);
+}
 
-    EVP_CIPHER_CTX *ctx;
-    int len;
-    int ciphertext_len;
-
-    ctx = EVP_CIPHER_CTX_new();
-
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, ciphertext_len;
     EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
-
     EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len);
     ciphertext_len = len;
-
     EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
     ciphertext_len += len;
-
     EVP_CIPHER_CTX_free(ctx);
-
     return ciphertext_len;
 }
 
 int main() {
-
     int sock;
     struct sockaddr_in server_address;
-
-    unsigned char hmac[EVP_MAX_MD_SIZE];
+    unsigned char hmac[EVP_MAX_MD_SIZE], iv[16], key[32];
     unsigned int hmac_len;
-
     int nonce;
 
-    unsigned char iv[16];
-    unsigned char key[32];
+    char username[50], password[50], password_hash[129] = {0};
 
+    printf("Enter Username: ");
+    scanf("%s", username);
+    printf("Enter Password: ");
+    scanf("%s", password);
+
+    hash_password(password, password_hash);
+    
     sock = socket(AF_INET, SOCK_STREAM, 0);
-
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    connect(sock, (struct sockaddr*)&server_address, sizeof(server_address));
-
-    printf("Connected to server\n");
+    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) return -1;
 
     srand(time(NULL));
     nonce = rand();
-
-    HMAC(EVP_sha256(),
-         PSK, strlen(PSK),
-         (unsigned char*)&nonce, sizeof(nonce),
-         hmac, &hmac_len);
-
+    HMAC(EVP_sha256(), PSK, strlen(PSK), (unsigned char*)&nonce, sizeof(nonce), hmac, &hmac_len);
     send(sock, &nonce, sizeof(nonce), 0);
     send(sock, hmac, 32, 0);
 
-    printf("CLIENT_HELLO sent\n");
+    char user_buf[50] = {0}, hash_buf[128] = {0};
+    strncpy(user_buf, username, 49);
+    strncpy(hash_buf, password_hash, 127);
+    send(sock, user_buf, 50, 0);
+    send(sock, hash_buf, 128, 0);
 
-    read(sock, iv, sizeof(iv));
-
-    printf("IV received\n");
+    if (read(sock, iv, sizeof(iv)) <= 0) {
+        printf("Authentication failed on server side.\n");
+        close(sock);
+        return -1;
+    }
 
     memcpy(key, PSK, 32);
-
-    char *message = "Hello secure server";
-
+    char *msg = "Hello secure server!";
     unsigned char ciphertext[BUFFER_SIZE];
+    int c_len = encrypt((unsigned char*)msg, strlen(msg), key, iv, ciphertext);
+    send(sock, ciphertext, c_len, 0);
 
-    int ciphertext_len = encrypt((unsigned char*)message,
-                                 strlen(message),
-                                 key,
-                                 iv,
-                                 ciphertext);
-
-    send(sock, ciphertext, ciphertext_len, 0);
-
-    printf("Encrypted message sent\n");
-
+    printf("Encrypted message sent successfully!\n");
     close(sock);
-
     return 0;
 }
--- server.c
+++ server.c
@@ -6,98 +6,111 @@
 #include <openssl/hmac.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
+#include <pthread.h> 
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define PSK "SuperSecretKey123SuperSecretKey12"
 
-void decrypt(unsigned char *ciphertext, int ciphertext_len,
-             unsigned char *key, unsigned char *iv,
-             unsigned char *plaintext) {
-
-    EVP_CIPHER_CTX *ctx;
-    int len;
-    int plaintext_len;
-
-    ctx = EVP_CIPHER_CTX_new();
-
+void decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, plaintext_len;
     EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
-
     EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len);
     plaintext_len = len;
-
     EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
     plaintext_len += len;
-
     plaintext[plaintext_len] = '\0';
-
     EVP_CIPHER_CTX_free(ctx);
 }
 
-int main() {
+void *handle_client(void *socket_desc) {
+    int new_socket = *(int*)socket_desc;
+    free(socket_desc);
 
+    unsigned char buffer[BUFFER_SIZE];
+    unsigned char client_hmac[32], server_hmac[EVP_MAX_MD_SIZE];
+    unsigned int hmac_len;
+    int nonce;
+    unsigned char iv[16], key[32];
+
+    read(new_socket, &nonce, sizeof(nonce));
+    read(new_socket, client_hmac, 32);
+    HMAC(EVP_sha256(), PSK, strlen(PSK), (unsigned char*)&nonce, sizeof(nonce), server_hmac, &hmac_len);
+
+    if (memcmp(client_hmac, server_hmac, 32) != 0) {
+        printf("HMAC Authentication failed\n");
+        close(new_socket);
+        return NULL;
+    }
+
+    char username[50] = {0}, password_hash[128] = {0};
+    read(new_socket, username, 50);
+    read(new_socket, password_hash, 128);
+
+    username[strcspn(username, "\r\n")] = 0;
+    password_hash[strcspn(password_hash, "\r\n")] = 0;
+
+    FILE *fptr = fopen("users.txt", "r");
+    if (!fptr) {
+        perror("Could not open users.txt");
+        close(new_socket);
+        return NULL;
+    }
+
+    char file_user[50], file_hash[128];
+    int auth_success = 0;
+    while (fscanf(fptr, "%s %s", file_user, file_hash) != EOF) {
+        if (strcmp(file_user, username) == 0 && strcmp(file_hash, password_hash) == 0) {
+            auth_success = 1;
+            break;
+        }
+    }
+    fclose(fptr);
+
+    if (!auth_success) {
+        printf("Login failed for: %s\n", username);
+        close(new_socket);
+        return NULL;
+    }
+    printf("User %s authenticated successfully\n", username);
+
+    RAND_bytes(iv, sizeof(iv));
+    send(new_socket, iv, sizeof(iv), 0);
+    memcpy(key, PSK, 32);
+
+    int ciphertext_len = read(new_socket, buffer, BUFFER_SIZE);
+    unsigned char plaintext[BUFFER_SIZE];
+    decrypt(buffer, ciphertext_len, key, iv, plaintext);
+    printf("Decrypted message: %s\n", plaintext);
+
+    close(new_socket);
+    return NULL;
+}
+
+int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    unsigned char buffer[BUFFER_SIZE];
-    unsigned char client_hmac[32];
-    unsigned char server_hmac[EVP_MAX_MD_SIZE];
-    unsigned int hmac_len;
-
-    int nonce;
-
-    unsigned char iv[16];
-    unsigned char key[32];
-
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
     bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-
-    listen(server_fd, 3);
-
+    listen(server_fd, 5);
     printf("Server listening on port %d\n", PORT);
 
-    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    while (1) {
+        new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = new_socket;
 
-    printf("Client connected\n");
-
-    read(new_socket, &nonce, sizeof(nonce));
-    read(new_socket, client_hmac, 32);
-
-    HMAC(EVP_sha256(),
-         PSK, strlen(PSK),
-         (unsigned char*)&nonce, sizeof(nonce),
-         server_hmac, &hmac_len);
-
-    if (memcmp(client_hmac, server_hmac, 32) != 0) {
-        printf("Authentication failed\n");
-        close(new_socket);
-        return 0;
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_client, (void*)new_sock);
+        pthread_detach(thread_id); 
     }
-
-    printf("Client authenticated\n");
-
-    RAND_bytes(iv, sizeof(iv));
-
-    send(new_socket, iv, sizeof(iv), 0);
-
-    memcpy(key, PSK, 32);
-
-    int ciphertext_len = read(new_socket, buffer, BUFFER_SIZE);
-
-    unsigned char plaintext[BUFFER_SIZE];
-
-    decrypt(buffer, ciphertext_len, key, iv, plaintext);
-
-    printf("Decrypted message: %s\n", plaintext);
-
-    close(new_socket);
     close(server_fd);
-
     return 0;
 }
